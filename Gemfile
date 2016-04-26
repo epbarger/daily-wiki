50b4ed5af1d0cbaf1cc37ec369893339e4b09400
Evan <evan@adharmonics.com>
2016-04-25 21:58:19 -0400

    convert Rails app to Sinatra

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+ruby "2.3.0"
+gem 'sinatra'
+gem 'haml'
+gem 'redis', '~>3.2'
+gem 'wikipedia-client'
+gem 'builder'
---
+++ web.rb
@@ -0,0 +1,114 @@
+require 'sinatra'
+require 'haml'
+require 'tilt/haml' # supress some console warning
+require 'wikipedia'
+require 'thread'
+require 'redis'
+require 'date'
+require 'yaml'
+require 'builder'
+
+NUMBER_OF_ARTICLES = 30
+
+get '/' do
+  set_articles
+  haml :index 
+end
+
+get '/feed' do
+  set_articles
+  builder :feed
+end
+
+def set_articles
+  redis = Redis.new(url: "redis://rediscloud:eqYBpy1EPatSySYK@pub-redis-17564.us-east-1-4.6.ec2.redislabs.com:17564")
+  @articles = redis.get(Date.today.to_s + '_v3')
+  if @articles
+    @articles = YAML.load(@articles)
+  else
+    threads = []
+    @articles = []
+    NUMBER_OF_ARTICLES.times do |i|
+      threads << Thread.new do
+        article = Wikipedia.find_random
+        while article.title.match(/list of/i) || (article.categories && article.categories.join.downcase.include?('disambiguation'))
+          article = Wikipedia.find_random
+        end
+        @articles << article
+      end 
+    end
+    threads.each(&:join)
+    redis.set(Date.today.to_s + '_v3', YAML.dump(@articles))
+  end
+
+  @articles = @articles.slice(0, NUMBER_OF_ARTICLES)
+end
+
+def truncate(string, size)
+  length = string.length
+  output = string[0..size].rstrip
+  if length > size
+    output + '...'
+  else
+    output
+  end
+end
+
+__END__
+
+@@index 
+!!!
+%html
+  %head
+    %title DailyWiki - New random wikipedia articles every day
+    %link{ rel: 'stylesheet', media: 'screen', href: '/styles.css' }
+    %link{ rel: 'stylesheet', media: 'screen', href: '/themes.css' }
+    %script{ src: '/script.js' }
+    %link{ rel: "alternate", type: "application/rss+xml", title: "DailyWiki RSS", href: "/rss" }
+  %body
+    .container
+      %header
+        %h1 
+          DailyWiki
+          %small
+            %a{ href: '/feed', class: 'feed_link'} rss
+        %span.controls
+          %span.theme-select.theme-default
+          %span.theme-select.theme-inverse
+      %main
+        - @articles.each_with_index do |article, index|
+          %a{ href: article.fullurl, title: truncate(article.summary, 667) }
+            %span.article-index= index+1
+            %span.article-title=article.title
+            »
+            = truncate(article.summary, 300)
+    :javascript
+      var body = document.getElementsByTagName("body")[0];
+      body.className = localStorage.theme;
+
+      var inverseThemeBtn = document.querySelectorAll(".theme-select.theme-inverse")[0];
+      inverseThemeBtn.addEventListener("click", function (e){ body.className = 'theme-inverse'; localStorage.theme = 'theme-inverse'}, false);
+
+      var defaultThemeBtn = document.querySelectorAll(".theme-select.theme-default")[0];
+      defaultThemeBtn.addEventListener("click", function (e){ body.className = ''; localStorage.theme = ''}, false);
+
+@@feed
+xml.instruct! :xml, version: "1.0" 
+xml.rss version: "2.0" do
+  xml.channel do
+    xml.title "DailyWiki"
+    xml.description "New random wikipedia articles every day"
+    xml.link uri('/')
+    xml.language 'en-us'
+    xml.pubDate DateTime.parse(Date.today.to_s).to_s
+    xml.lastBuildDate DateTime.parse(Date.today.to_s).to_s
+
+    @articles.each do |article|
+      xml.item do
+        xml.title article.title
+        xml.link article.fullurl
+        xml.description truncate(article.summary, 667)
+      end
+    end
+  end
+end
